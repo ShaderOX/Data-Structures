c83471f669461c3a1c42ae76413514ee98e30905
Asadullah Kashif <s.asad18k@gmail.com>
2021-04-09 19:57:54 +0500

    Added docs. for Single Linked List

--- data-structures.cpp
+++ data-structures.cpp
@@ -4,12 +4,27 @@
 #include <Vector.h>
 
 int main() {
-    Vector<int> vector(5);
-    for (int i = 0; i < 25; i++) {
-        vector.append(pow(i, 2));
-    }
 
-    std::cout << vector << std::endl;
-    std::cout << vector.getSize() << std::endl;
-    std::cout << vector[24] << std::endl;
+    LinkedList<int> ll;
+    ll.add(10);
+    ll.add(12);
+    ll.add(13);
+    ll.add(15);
+    std::cout << ll << std::endl;
+    std::cout << ll.getSize() << std::endl;
+    ll.remove(10);
+    std::cout << ll << std::endl;
+    std::cout << ll.getSize() << std::endl;
+    ll.remove(15);
+    std::cout << ll << std::endl;
+    std::cout << ll.getSize() << std::endl;
+
+    // Vector<int> vector(5);
+    // for (int i = 0; i < 25; i++) {
+    //     vector.append(pow(i, 2));
+    // }
+
+    // std::cout << vector << std::endl;
+    // std::cout << vector.getSize() << std::endl;
+    // std::cout << vector[24] << std::endl;
 }
--- src/Singly Linked List/SinglyLinkedList.h
+++ src/Singly Linked List/SinglyLinkedList.h
@@ -4,22 +4,29 @@
 template <typename T>
 class LinkedList {
 private:
+    // Will be a LinkedList Node
     struct Node {
         T m_value;
         Node *m_next;
-        Node(T value, Node *next) {
-            m_value = value;
-            m_next = next;
-        }
+        Node(T value, Node *next)
+            : m_value(value), m_next(next) {}
     };
 
-    Node *head = nullptr;
-    size_t length = 0;
-
+    Node *head = nullptr;   // Holds the pointer to the head of linked list
+    size_t size = 0;      // Holds the length of Nodes in the LinkedList
 public:
+    /**
+     * @brief Adds the a Node to the end of the LinkedList.
+     *
+     * @param value
+     */
     void add(const T &value) {
-        if (head == nullptr) { head = new Node(value, nullptr); }
+        // If its the first Node to be added then it'll be the head.
+        if (head == nullptr) {
+            head = new Node(value, nullptr);
+        }
         else {
+            // Iterates over each Node, and till it finds a nullptr and then adds a new Node.
             for (Node *tmp = head;; tmp = tmp->m_next) {
                 if (tmp->m_next == nullptr) {
                     tmp->m_next = new Node(value, nullptr);
@@ -27,14 +34,22 @@ public:
                 }
             }
         }
-        length++;
+        // Updates the length of the LinkedList
+        size++;
     }
 
+    /**
+     * @brief Gets the size of the LinkedList
+     *
+     * @return size_t
+     */
+    size_t getSize() { return size; }
 
-    size_t getSize() {
-        return length;
-    }
-
+    /**
+     * @brief Removes a certain Node with the given value from the LinkedList
+     *
+     * @param value
+     */
     void remove(const T &value) {
         Node *tmp = head;
         // If the item being removed is the head of the list 
@@ -42,29 +57,45 @@ public:
             tmp = head->m_next;
             delete head;
             head = tmp;
-            return;
         }
-        Node *tmpPrev = head;
-        tmp = head->m_next;
-        for (;;) {
-            // If the item being removed is anywhere other than the head of the list
-            if (tmp == nullptr) { return; }
-            if (tmp->m_value == value) {
-                tmpPrev->m_next = tmp->m_next;
-                delete tmp;
-                length--;
-                return;
+        // If the item being removed is anywhere other than the head of the list
+        else {
+            Node *tmpPrev = head;
+            tmp = head->m_next;
+            for (;;tmpPrev = tmp, tmp = tmp->m_next) {
+                // In case if the Node with the given value is non-existant
+                if (tmp == nullptr) { return; }
+
+                // If it finds the node, then adjust the next pointer of the previous Node and frees the memory.
+                if (tmp->m_value == value) {
+                    tmpPrev->m_next = tmp->m_next;
+                    delete tmp;
+                    break;
+                }
             }
-            tmpPrev = tmp;
-            tmp = tmp->m_next;
         }
 
+        // Updates the size
+        size--;
     }
 
+    /**
+     * @brief Returns whether a Node with the given value exists in the LinkedList
+     *
+     * @param value
+     * @return true
+     * @return false
+     */
     bool contains(const T &value) {
         return indexOf(value) != -1;
     }
 
+    /**
+     * @brief Returns the index of the Node with the given value in case it exists or -1 it case it doesn't
+     *
+     * @param value
+     * @return int
+     */
     int indexOf(const T &value) {
         Node *tmp = head;
         int counter = 0;
@@ -81,17 +112,28 @@ public:
         }
     }
 
+    /**
+     * @brief Destroy the LinkedList object and frees all the allocated memory
+     *
+     */
     ~LinkedList() {
         LinkedList<T>::Node *tmp = head;
         LinkedList<T>::Node *tmpNext = head->m_next;
-        for (;;) {
+        // Iterates over if the entire LinkedList and frees the memory
+        for (;;tmp = tmpNext, tmpNext = tmpNext->m_next) {
             if (tmpNext == nullptr) { break; }
             delete tmp;
-            tmp = tmpNext;
-            tmpNext = tmpNext->m_next;
         }
     }
 
+    /**
+     * @brief Returns a string printable form of the LinkedList object
+     *
+     * @tparam A
+     * @param stream
+     * @param ll
+     * @return std::ostream&
+     */
     template <typename A>
     friend std::ostream &operator<<(std::ostream &stream, const LinkedList<A> &ll) {
         Node *tmp = ll.head;
@@ -99,7 +141,7 @@ public:
         for (;;) {
             if (tmp == nullptr) { stream << std::endl; break; }
             stream << tmp->m_value;
-            if (counter < (ll.length - 1)) { stream << " -> "; }
+            if (counter < (ll.size - 1)) { stream << " -> "; }
             tmp = tmp->m_next;
             counter++;
         }
